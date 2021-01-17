c091d07e891df7bddc33b9562b87abe491b92ad1
Allan Reuben <allanreuben99@gmail.com>
2021-01-17 15:27:45 -0500

    Wrote test suite for expected outcomes

--- Assignment 1/fsm_tb.vhd
+++ Assignment 1/fsm_tb.vhd
@@ -39,12 +39,91 @@ END PROCESS;
 --TODO: Thoroughly test your FSM
 stim_process: PROCESS
 BEGIN    
-	REPORT "Example case, reading a meaningless character";
+	REPORT "Case moving from state 0 to 0, expected output 0";
 	s_input <= "01011000";
 	WAIT FOR 1 * clk_period;
 	ASSERT (s_output = '0') REPORT "When reading a meaningless character, the output should be '0'" SEVERITY ERROR;
-	REPORT "_______________________";
-    
+
+	REPORT "Case moving from state 0 to 1, expected output 0";
+	s_input <= "00101111"; -- slash character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After first state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 1 to 2, expected output 0";
+	s_input <= "00101010"; -- star character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 2 to 3, expected output 1";
+	s_input <= "00101010"; -- star character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+	
+	REPORT "Case moving from state 3 to 3, expected output 1";
+	s_input <= "00101010"; -- star character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After no state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 3 to 4, expected output 1";
+	s_input <= "01111110"; -- meaningless character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 4 to 4, expected output 1";
+	s_input <= "01111110"; -- meaningless character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After no state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 4 to 3, expected output 1";
+	s_input <= "00101010"; -- star character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 3 to 7, expected output 1";
+	s_input <= "00101111"; -- slash character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 7 to 0, expected output 0";
+	s_input <= "01111110"; -- meaningless character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 0 to 0 with new line, expected output 0";
+	s_input <= "00001010"; -- new line character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After no state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 0 to 1, expected output 0";
+	s_input <= "00101111"; -- slash character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After first state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 1 to 5, expected output 0";
+	s_input <= "00101111"; -- slash character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After state transition, output should be '0'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 5 to 6, expected output 1";
+	s_input <= "01111110"; -- meaningless character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 6 to 6, expected output 1";
+	s_input <= "01111110"; -- meaningless character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After no state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 6 to 7, expected output 1";
+	s_input <= "00001010"; -- new line character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "After state transition, output should be '1'" SEVERITY ERROR;
+
+	REPORT "Case moving from state 7 to 1, expected output 0";
+	s_input <= "00101111"; -- slash character
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "After state transition, output should be '0'" SEVERITY ERROR;
+
 	WAIT;
 END PROCESS stim_process;
 END;
