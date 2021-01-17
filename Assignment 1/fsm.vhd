685c37fe07848b41dfe7917306fd90691732ce66
Noah-C-Z <nczwack@gmail.com>
2021-01-17 16:17:18 -0500

    Fix FSM code so that output works

--- Assignment 1/fsm.vhd
+++ Assignment 1/fsm.vhd
@@ -18,85 +18,90 @@ constant SLASH_CHARACTER : std_logic_vector(7 downto 0) := "00101111";
 constant STAR_CHARACTER : std_logic_vector(7 downto 0) := "00101010";
 constant NEW_LINE_CHARACTER : std_logic_vector(7 downto 0) := "00001010";
 
-signal state : std_logic_vector(2 downto 0) := "000";
+signal state	: std_logic_vector(2 downto 0) := "000";
+signal tempOut	: std_logic := '0';
 
 begin
 
 -- Insert your processes here
 process (clk, reset)
 begin
-    if (reset = '0') then
+    if (reset = '1') then
         state <= "000";
+		tempOut <= '0';
     elsif (rising_edge(clk)) then
         case (state) is
             when "000" =>
                 if (input = SLASH_CHARACTER) then
                     state <= "001";
-                    output <= '0';
+                    tempOut <= '0';
                 else
                     state <= "000";
-                    output <= '0';
+                    tempOut <= '0';
                 end if;
             when "001" =>
                 if (input = STAR_CHARACTER) then
                     state <= "010";
-                    output <= '0';
+                    tempOut <= '0';
                 elsif (input = SLASH_CHARACTER) then
                     state <= "101";
-                    output <= '0';
+                    tempOut <= '0';
                 else
                     state <= "000";
-                    output <= '0';
+                    tempOut <= '0';
                 end if;
             when "010" =>
                 if (input = STAR_CHARACTER) then
                     state <= "011";
-                    output <= '1';
+                    tempOut <= '1';
                 else
                     state <= "100";
-                    output <= '1';
+                    tempOut <= '1';
                 end if;
             when "011" =>
                 if (input = STAR_CHARACTER) then
                     state <= "011";
-                    output <= '1';
+                    tempOut <= '1';
                 elsif (input = SLASH_CHARACTER) then
                     state <= "111";
-                    output <= '1';
+                    tempOut <= '1';
                 else
                     state <= "100";
-                    output <= '1';
+                    tempOut <= '1';
                 end if;
             when "100" =>
                 if (input = STAR_CHARACTER) then
                     state <= "011";
-                    output <= '1';
+                    tempOut <= '1';
                 else
                     state <= "100";
-                    output <= '1';
+                    tempOut <= '1';
                 end if;
             when "101" | "110" =>
                 if (input = NEW_LINE_CHARACTER) then
                     state <= "111";
-                    output <= '1';
+                    tempOut <= '1';
                 else
                     state <= "110";
-                    output <= '1';
+                    tempOut <= '1';
                 end if;
             when "111" =>
                 if (input = SLASH_CHARACTER) then
                     state <= "001";
-                    output <= '0';
+                    tempOut <= '0';
                 else
                     state <= "000";
-                    output <= '0';
+                    tempOut <= '0';
                 end if;
             when others =>
                 state <= state;
         end case;
     else
         state <= state;
+		tempOut <= tempOut;
     end if;
 end process;
+
+output <= tempOut;
 
 end behavioral;
