a4a59b2346a15749b3afadf8e0b3d888e6c24194
Noah-C-Z <nczwack@gmail.com>
2021-01-17 14:55:48 -0500

    Implement state machine in VHDL

--- Assignment 1/fsm.vhd
+++ Assignment 1/fsm.vhd
@@ -18,12 +18,85 @@ constant SLASH_CHARACTER : std_logic_vector(7 downto 0) := "00101111";
 constant STAR_CHARACTER : std_logic_vector(7 downto 0) := "00101010";
 constant NEW_LINE_CHARACTER : std_logic_vector(7 downto 0) := "00001010";
 
+signal state : std_logic_vector(3 downto 0) := "000";
+
 begin
 
 -- Insert your processes here
 process (clk, reset)
 begin
-    output <= clk;
+    if (reset = '0') then
+        state <= "000";
+    elsif (rising_edge(clk)) then
+        case (state) is
+            when "000" =>
+                if (input = SLASH_CHARACTER) then
+                    state <= "001";
+                    output <= '0';
+                else
+                    state <= "000";
+                    output <= '0';
+                end if;
+            when "001" =>
+                if (input = STAR_CHARACTER) then
+                    state <= "010";
+                    output <= '0';
+                elsif (input = SLASH_CHARACTER) then
+                    state <= "101";
+                    output <= '0';
+                else
+                    state <= "000";
+                    output <= '0';
+                end if;
+            when "010" =>
+                if (input = STAR_CHARACTER) then
+                    state <= "011";
+                    output <= '1';
+                else
+                    state <= "100";
+                    output <= '1';
+                end if;
+            when "011" =>
+                if (input = STAR_CHARACTER) then
+                    state <= "011";
+                    output <= '1';
+                elsif (input = SLASH_CHARACTER) then
+                    state <= "111";
+                    output <= '1';
+                else
+                    state <= "100";
+                    output <= '1';
+                end if;
+            when "100" =>
+                if (input = STAR_CHARACTER) then
+                    state <= "011";
+                    output <= '1';
+                else
+                    state <= "100";
+                    output <= '1';
+                end if;
+            when "101" or "110" =>
+                if (input = NEW_LINE_CHARACTER) then
+                    state <= "111";
+                    output <= '1';
+                else
+                    state <= "110";
+                    output <= '1';
+                end if;
+            when "111" =>
+                if (input = SLASH_CHARACTER) then
+                    state <= "001";
+                    output <= '0';
+                else
+                    state <= "000";
+                    output <= '0';
+                end if;
+            when others =>
+                state <= state;
+        end case;
+    else
+        state <= state;
+    end if;
 end process;
 
 end behavioral;
