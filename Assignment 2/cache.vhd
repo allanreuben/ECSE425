227ba5168487db89c766f0c6dcc4d02f277ea1f0
Noah-C-Z <nczwack@gmail.com>
2021-02-20 18:26:19 -0500

    Refactor cache to use words and implement cache read

--- Assignment 2/cache.vhd
+++ Assignment 2/cache.vhd
@@ -29,14 +29,19 @@ end cache;
 
 architecture arch of cache is
 	-- Size of cache in bytes and blocks
-	constant CACHE_SIZE_BYTES: natural := 512;
-	constant BYTES_PER_BLOCK: natural := 16;
-	constant CACHE_SIZE_BLOCKS: natural := CACHE_SIZE_BYTES / BYTES_PER_BLOCK;
-	-- Size of tag and offset in bits
-	constant TAG_SIZE: natural := 11;
-	constant OFFSET_SIZE: natural := 2;
+	constant CACHE_SIZE_WORDS: natural := 128;
+	constant WORDS_PER_BLOCK: natural := 4;
+	constant CACHE_SIZE_BLOCKS: natural := CACHE_SIZE_WORDS / WORDS_PER_BLOCK;
+	-- Location where useful address data begins
+	constant ADDRESS_START: natural := 14;
+	-- Locations of the last bit of the tag, block address, and block offset
+	constant TAG_END_BIT: natural := 9;
+	constant BLOCK_ADDR_END_BIT: natural := 4;
+	constant OFFSET_END_BIT: natural := 2;
+	-- Some sizes
+	constant TAG_SIZE: natural := ADDRESS_START - TAG_END_BIT + 1;
 	-- An array type for the data in the cache
-	type cache_data is array(CACHE_SIZE_BYTES-1 downto 0) of std_logic_vector(7 downto 0);
+	type cache_data is array(CACHE_SIZE_WORDS-1 downto 0) of std_logic_vector(31 downto 0);
 	-- An array type for the tags in the cache
 	type cache_tags is array(CACHE_SIZE_BLOCKS-1 downto 0) of std_logic_vector(TAG_SIZE-1 downto 0);
 	-- An array type for the valid(1)/invalid(0) and dirty(1)/clean(0) flags
@@ -48,17 +53,20 @@ architecture arch of cache is
 	signal cache_f: cache_flags;
 	-- Wait request signal
 	signal waitreq_reg: std_logic := '1';
-	signal tag_reg: std_logic_vector(TAG_SIZE-1 downto 0);
-	-- Variables
-	variable block_idx: natural;
+	-- Additional signals
+	signal readdata: std_logic_vector(31 downto 0); -- The data to be read by the processor
+	signal memread: std_logic := '0'; -- High when reading from main memory
+	signal memwrite: std_logic := '0'; -- High when writing to main memory
 begin
 
 	cache_proc: process (clock, reset)
+		variable block_idx: natural range 0 to CACHE_SIZE_BLOCKS-1;
+		variable tag: std_logic_vector(TAG_SIZE-1 downto 0);
 	begin
 		-- Initialize the arrays
 		if (reset = '1') or (now < 1 ps) then
-			for i in 0 to CACHE_SIZE_BYTES-1 loop
-				cache_d(i) <= "00000000";
+			for i in 0 to CACHE_SIZE_WORDS-1 loop
+				cache_d(i) <= "00000000000000000000000000000000";
 			end loop;
 			-- Initialize the tags and flags
 			for i in 0 to CACHE_SIZE_BLOCKS-1 loop
@@ -69,16 +77,22 @@ begin
 
 		-- Main processing block
 		elsif (rising_edge(clock)) then
-			if (s_read) then
-				tag_reg <= s_addr(14 downto 14-TAG_SIZE+1);
-				block_idx := to_integer(unsigned(tag_reg)) mod CACHE_SIZE_BLOCKS;
+			if (waitreq_reg = '0') then
+				waitreq_reg <= '1';
+			elsif (s_read) then
+				tag := s_addr(ADDRESS_START downto TAG_END_BIT);
+				block_idx := to_integer(unsigned(s_addr(TAG_END_BIT-1 downto BLOCK_ADDR_END_BIT)));
+				offset := to_integer(unsigned(s_addr(BLOCK_ADDR_END_BIT-1 downto OFFSET_END_BIT)));
 				-- Check if tag matches
-				if (tag_reg = cache_t(block_idx)) then
+				if (tag = cache_t(block_idx)) then
 					-- Check if block is valid
 					if (cache_f(block_idx)(1) = '1') then
 						-- Return data found at that address in cache
+						readdata <= cache_d(block_idx + offset);
+						waitreq_reg <= '0';
 					else
 						-- Request the data from the main memory
+
 					end if;
 				else
 					-- Check if block is dirty
@@ -91,10 +105,11 @@ begin
 					end if;
 				end if;
 			elsif (s_write) then
-				tag_reg <= s_addr(14 downto 14-TAG_SIZE+1);
-				block_idx := to_integer(unsigned(tag_reg)) mod CACHE_SIZE_BLOCKS;
+				tag := s_addr(14 downto 14-TAG_SIZE+1);
+				-- block_idx := to_integer(unsigned(tag)) mod CACHE_SIZE_BLOCKS;
+				block_idx := s_addr(14-TAG_SIZE downto 14-(TAG_SIZE + BLOCK_ADDR_SIZE)+1);
 				-- Check if tag matches
-				if (tag_reg = cache_t(block_idx)) then
+				if (tag = cache_t(block_idx)) then
 					-- Check if block is valid
 					if (cache_f(block_idx)(1) = '1') then
 						-- Write the data into the cache block
@@ -120,8 +135,8 @@ begin
 				-- Just vibe
 			end if;
 		end if;
-
-		s_waitrequest <= waitreq_reg;
 	end process;
 
+	s_waitrequest <= waitreq_reg;
+	s_readdata <= readdata;
 end arch;
